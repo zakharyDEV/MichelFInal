514ef7208a0edda8a806bf2db11ac55f4031e2be
zakhary brassard <151887022+zakharyDEV@users.noreply.github.com>
2024-02-01 19:33:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 
 // Redirect the root path to "register.html"
 app.get('/', (req, res) => {
-  res.redirect('public/register.html');
+  res.redirect('/register.html');
 });
+
+app.use(express.static('public'));
 
 module.exports = app;
