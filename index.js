bec2c3c958d091c3df8ac89f77e949749aa4b513
zakhary brassard <151887022+zakharyDEV@users.noreply.github.com>
2024-02-01 19:40:59 -0500

    tfdgfe

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// index.js
 const express = require('express');
 const app = express();
 
-// Serve static files from the "public" directory
+// Redirect the root path to "register.html"
+app.get('/', (req, res) => {
+  res.redirect('/register.html');
+});
+
 app.use(express.static('public'));
 
 module.exports = app;
